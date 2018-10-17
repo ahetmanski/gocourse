7092f52f95ba945e1978bf19d26df88b5b5f768f
Andrei Hetmanski <andrei.hetmanski@gmail.com>
2018-10-17 19:29:58 +0300

    Add `metrics` structure to `person`

--- structure/main.go
+++ structure/main.go
@@ -4,21 +4,25 @@ import "fmt"
 
 type person struct {
 	name string
-	age  int
+	m    metrics
+}
+
+type metrics struct {
+	age int
 }
 
 func main() {
-	bob := person{"Bob", 20}
+	bob := person{"Bob", metrics{20}}
 	alice := &person{name: "Alice"}
 
-	fmt.Println(alice.name, alice.age)
+	fmt.Println(alice.name, alice.m.age)
 
-	bob.age = 32
+	bob.m.age = 32
 	fmt.Println(bob)
 
-	alice.age = 51
+	alice.m.age = 51
 	fmt.Println(alice)
 
-	(*alice).age = 62
+	(*alice).m.age = 62
 	fmt.Println(alice)
 }
